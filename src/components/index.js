298beeaab456208765228df8f35068d8eea4f9f4
Atıf Şimşek <dev.atifsimsek@gmail.com>
2023-01-13 12:41:10 +0300

    components and styles files created

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header";
+export { default as Inputs } from "./Inputs";
+export { default as Filter } from "./Filter";
+export { default as Footer } from "./Footer";
+export { default as List } from "./List";
